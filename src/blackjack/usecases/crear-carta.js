d084ce9e1b585bad873b72714f00e9b7928b0e68
fabian lopez <f.ls9323@gmail.com>
2025-01-25 18:44:07 -0500

    cambiando rutas

--- src/blackjack/usecases/crear-carta.js
+++ src/blackjack/usecases/crear-carta.js
@@ -4,7 +4,7 @@ export const crearCarta = (carta, turno, divCartasJugadores)=>{
 
     const crearCarta = document.createElement('img');
         // crearCarta.src= ` assets/cartas/${carta}.png`;
-        crearCarta.src= ` public/assets/cartas/${carta}.png`;
+        crearCarta.src= ` docs/assets/cartas/${carta}.png`;
         console.log(crearCarta)
         crearCarta.classList.add('carta')
         console.log(divCartasJugadores)
